c3be2db13f753f2251a4fe12bda92a3bde154f34
celiflulan <92764504+celiflulan@users.noreply.github.com>
2024-03-31 19:13:43 +0800

    Add files via upload

---
+++ programmer-club-auth-application/programmer-club-auth-application-controller/src/main/java/com/programmer/auth/application/convert/AuthUserDTOConverter.java
@@ -0,0 +1,21 @@
+package com.jingdianjichi.auth.application.convert;
+
+import com.jingdianjichi.auth.domain.entity.AuthUserBO;
+import com.jingdianjichi.auth.entity.AuthUserDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * 用户dto转换器
+ *
+ */
+@Mapper
+public interface AuthUserDTOConverter {
+
+    AuthUserDTOConverter INSTANCE = Mappers.getMapper(AuthUserDTOConverter.class);
+
+    AuthUserBO convertDTOToBO(AuthUserDTO authUserDTO);
+
+    AuthUserDTO convertBOToDTO(AuthUserBO authUserBO);
+
+}
---
+++ programmer-club-auth-application/programmer-club-auth-application-controller/src/main/java/com/programmer/auth/application/dto/AuthRoleDTO.java
@@ -0,0 +1,22 @@
+package com.jingdianjichi.auth.application.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 角色dto
+ *
+ */
+@Data
+public class AuthRoleDTO implements Serializable {
+
+    private Long id;
+    
+    private String roleName;
+    
+    private String roleKey;
+
+}
+
