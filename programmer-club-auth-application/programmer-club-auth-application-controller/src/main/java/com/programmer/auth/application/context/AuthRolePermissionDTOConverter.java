452b3155a048757dee660399ed9675f1fbd8f053
celiflulan <92764504+celiflulan@users.noreply.github.com>
2024-03-31 19:22:15 +0800

    Add files via upload

---
+++ programmer-club-auth-application/programmer-club-auth-application-controller/src/main/java/com/programmer/auth/application/context/AuthRolePermissionDTOConverter.java
@@ -0,0 +1,21 @@
+package com.jingdianjichi.auth.application.convert;
+
+import com.jingdianjichi.auth.application.dto.AuthPermissionDTO;
+import com.jingdianjichi.auth.application.dto.AuthRolePermissionDTO;
+import com.jingdianjichi.auth.domain.entity.AuthPermissionBO;
+import com.jingdianjichi.auth.domain.entity.AuthRolePermissionBO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+/**
+ * 权限dto转换器
+ *
+ */
+@Mapper
+public interface AuthRolePermissionDTOConverter {
+
+    AuthRolePermissionDTOConverter INSTANCE = Mappers.getMapper(AuthRolePermissionDTOConverter.class);
+
+    AuthRolePermissionBO convertDTOToBO(AuthRolePermissionDTO authRolePermissionDTO);
+
+}
